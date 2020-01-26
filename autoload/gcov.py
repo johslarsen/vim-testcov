03b1632eb8e284e9d1502c4d7ca2c56983ddc940
Johannes Larsen <mail@johslarsen.net>
2020-01-26 14:05:37 +0100

    WIP: Read source filenames from gcno metadata

--- autoload/gcov.py
+++ autoload/gcov.py
@@ -53,3 +53,47 @@ def gcno_read_uint32(gcno_file):
 def gcno_read_str(gcno_file):
     strlen_in_word = gcno_read_uint32(gcno_file)
     return gcno_file.read(strlen_in_word*4).decode().rstrip("\x00")
+
+def gcno_cwd_and_absolute_source(gcno_path):
+    with open(gcno_path, "rb") as f:
+        f.seek(12)
+        cwd = gcno_read_str(f)
+        f.seek(4,1)
+
+        srcs = set()
+        while True:
+            (tag, offset_next) = gcno_read_tag(f)
+            if tag is None:
+                break
+            elif tag == 0x01000000: # GCOV_TAG_FUNCTION
+                srcs.add(gcno_src_from_function(cwd, f))
+            elif tag == 0x01450000: # GCOV_TAG_LINES
+                srcs |= gcno_srcs_from_lines(cwd, f)
+
+            f.seek(offset_next)
+    return (cwd, srcs)
+
+def gcno_read_tag(gcno_file):
+    tag = gcno_read_uint32(gcno_file)
+    if tag == 0:
+        return (None, 0)
+    uint32_len = gcno_read_uint32(gcno_file)
+    return (tag, gcno_file.tell() + uint32_len*4)
+
+def gcno_src_from_function(cwd, gcno_file):
+    gcno_file.seek(12, 1)
+    _function_name = gcno_read_str(gcno_file)
+    gcno_file.seek(4, 1)
+    return gcno_absolute_path(cwd, gcno_read_str(gcno_file))
+
+def gcno_srcs_from_lines(cwd, gcno_file):
+    srcs = set()
+    gcno_file.seek(4, 1)
+    while True:
+        linenr = gcno_read_uint32(gcno_file)
+        if linenr != 0:
+            continue
+        filename = gcno_read_str(gcno_file)
+        if not filename:
+            return srcs
+        srcs.add(gcno_absolute_path(cwd, filename))
